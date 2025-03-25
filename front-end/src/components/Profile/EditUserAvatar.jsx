a28f9ef70afc830dd8e20db4def86cb360c1233d
Latharagav <tgpremlatha@gmail.com>
2025-03-25 15:21:24 -0500

    added delete button to avatar

--- front-end/src/components/Profile/EditUserAvatar.jsx
+++ front-end/src/components/Profile/EditUserAvatar.jsx
@@ -2,14 +2,34 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { useState } from "react";
-import { Box, CardActions, IconButton } from "@mui/material";
+import { Box, CardActions, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { Card, CardContent, Typography } from "@mui/material";
 import getInitials from '../Util.js';
 
 export default function EditUserAvatar({user, refreshUser, setAlert }) {
   const [image, setImage] = useState(null);
   const [file, setFile] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [selectedPic, setSelectedPic] = useState(null);
+
+  const handleDeleteClick = () => {
+    setOpen(true);
+    setSelectedPic(user.profileUrl);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedPic(null);
+  };
+
+  const handleDeleteAttraction = () => {
+    if (selectedPic) {
+      handleDelete();
+    }
+    setOpen(false);
+  }
 
   const initials = getInitials(user);
 
@@ -50,6 +70,28 @@ export default function EditUserAvatar({user, refreshUser, setAlert }) {
     document.getElementById("file-input").click();
   };
 
+
+  const handleDelete = async() => {
+
+    try{
+      await fetch(`http://localhost:8080/userprofileservices/${user.id}/picture`,
+        {
+          method: "DELETE",
+          credentials: "include"
+        }
+      );
+
+      setImage(null);
+      setAlert({
+        ...alert,
+        message: "Avatar delete successful!"
+      });
+      refreshUser();
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
   return (
     <>
       <Card sx={{ minWidth: 250, mt: 5, bgcolor:'#F2E8DC' }}>
@@ -65,8 +107,9 @@ export default function EditUserAvatar({user, refreshUser, setAlert }) {
             variant="h5"
             gutterBottom
           >
-            My Avatar
+            My Avatar {user.profileUrl}
           </Typography>
+        
 
           <Box
             sx={{
@@ -75,6 +118,25 @@ export default function EditUserAvatar({user, refreshUser, setAlert }) {
               position: "relative"
             }}
           >
+            {
+              user.profileUrl != null && <IconButton
+              size="large"
+              aria-label="edit"
+              sx={{
+                position: "relative",
+                top: 70,
+                left: 8,
+                backgroundColor: "white",
+                borderRadius: "50%",
+                width: 40,
+                height: 40
+              }}
+              onClick={handleDeleteClick}
+            >
+              <DeleteIcon fontSize="medium"></DeleteIcon>
+            </IconButton>
+            }
+            
             <Avatar
               sx={{
                 width: 112,
@@ -84,13 +146,14 @@ export default function EditUserAvatar({user, refreshUser, setAlert }) {
             >
               {initials}
             </Avatar>
+            
             <IconButton
               size="large"
               aria-label="edit"
               sx={{
                 position: "relative",
                 top: 70,
-                right: 0,
+                right: 8,
                 backgroundColor: "white",
                 borderRadius: "50%",
                 width: 40,
@@ -98,7 +161,7 @@ export default function EditUserAvatar({user, refreshUser, setAlert }) {
               }}
               onClick={handleEditClick}
             >
-              <EditIcon fontSize="large"></EditIcon>
+              <EditIcon fontSize="medium"></EditIcon>
             </IconButton>
             <input
               id="file-input"
@@ -127,6 +190,25 @@ export default function EditUserAvatar({user, refreshUser, setAlert }) {
           </CardActions>
         </CardContent>
       </Card>
+      <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                  >
+                    <DialogTitle id="alert-dialog-title">
+                      {"Confirm Avatar Deletion"}
+                    </DialogTitle>
+                    <DialogContent>
+                      <DialogContentText id="alert-dialog-description">
+                        Are you sure you want to delete your avatar?
+                      </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                      <Button onClick={handleClose}>Cancel</Button>
+                      <Button onClick={handleDeleteAttraction}>Yes</Button>
+                    </DialogActions>
+                  </Dialog>
     </>
   );
 }
